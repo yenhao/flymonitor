cdaaa8fb5b38faf127ba522a02c833809a36f160
yenhao <yenhao0218@gmail.com>
2017-06-03 17:52:20 +0800

    disable node_modules cache

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var linebot = require('linebot');
 var express = require('express');
-var getJSON = require('get-json');
+// var getJSON = require('get-json');
 
+// var api_basic = 'http://zcash.flypool.org/api/miner_new/';
 
 var bot = linebot({
   channelId: "1518261464",
@@ -9,13 +10,20 @@ var bot = linebot({
   channelAccessToken: "HQG7JamlwUynuUqynTh7mWBRoBLrLx/+RjQoriGD4FMhpexOEXQ3QV2n1iDgY0XqSmne96+U3xhC2vo+wz6tadNPNQSnkrSH6X7cTGD7s2zDQ/QFmXeAzS7hS1IruN5J437X6IeuIaWbApSNLsZ/hQdB04t89/1O/w1cDnyilFU="
 });
 
+// _bot();
+bot.on('message', function(event) {
+  if (event.message.type = 'text') {
+    var msg = event.message.text;
+    event.reply(msg).then(function(data) {
+      // success 
+      console.log(msg);
+    }).catch(function(error) {
+      // error 
+      console.log('error');
+    });
+  }
+});
 
-
-var timer;
-var pm = [];
-_getJSON();
-
-_bot();
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
@@ -26,44 +34,19 @@ var server = app.listen(process.env.PORT || 8080, function() {
   console.log("App now running on port", port);
 });
 
-function _bot() {
-  bot.on('message', function(event) {
-    if (event.message.type == 'text') {
-      var msg = event.message.text;
-      var replyMsg = '';
-      if (msg.indexOf('PM2.5') != -1) {
-        pm.forEach(function(e, i) {
-          if (msg.indexOf(e[0]) != -1) {
-            replyMsg = e[0] + '的 PM2.5 數值為 ' + e[1];
-          }
-        });
-        if (replyMsg == '') {
-          replyMsg = '請輸入正確的地點';
-        }
-      }
-      if (replyMsg == '') {
-        replyMsg = '不知道「'+msg+'」是什麼意思 :p';
-      }
 
-      event.reply(replyMsg).then(function(data) {
-        console.log(replyMsg);
-      }).catch(function(error) {
-        console.log('error');
-      });
-    }
-  });
 
-}
+// function _bot(){
+//   bot.on('message', function(event) {
+//     var account = event.message.text;
+//     console.log(account);
+//     // _getJSON(account);
+//   });
+// }
 
-function _getJSON() {
-  clearTimeout(timer);
-  getJSON('http://opendata2.epa.gov.tw/AQX.json', function(error, response) {
-    response.forEach(function(e, i) {
-      pm[i] = [];
-      pm[i][0] = e.SiteName;
-      pm[i][1] = e['PM2.5'] * 1;
-      pm[i][2] = e.PM10 * 1;
-    });
-  });
-  timer = setInterval(_getJSON, 1800000); //每半小時抓取一次新資料
-}
+// function _getJSON(account) {
+//   // getJSON( api_basic + account, function(error, response) {
+//     console.log(response);
+//   // });
+//   // timer = setInterval(_getJSON, 1800000); //每半小時抓取一次新資料
+// }
